736aff72e00e7a721379e38748de1c50c90d851a
Guillem <guillem.callau@gmail.com>
2021-11-12 19:18:39 +0100

    Controler

--- src/main/java/com/iscariotstudio/restauranteapi/controller/MainController.java
+++ src/main/java/com/iscariotstudio/restauranteapi/controller/MainController.java
@@ -1,4 +1,23 @@
 package com.iscariotstudio.restauranteapi.controller;
 
+import com.iscariotstudio.restauranteapi.exception.MainException;
+import com.iscariotstudio.restauranteapi.json.RestauranteRest;
+import com.iscariotstudio.restauranteapi.responses.RestauranteResponse;
+import com.iscariotstudio.restauranteapi.service.RestauranteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/restauranteApi" + "v1")
 public class MainController {
+
+     @Autowired
+    RestauranteService restauranteService;
+
+     public RestauranteResponse<RestauranteRest> getRestauranteById(Long id) throws MainException {
+         return new RestauranteResponse<>("Succes", String.valueOf(HttpStatus.OK), "OK",
+                 restauranteService.getRestauranteById(id));
+     }
 }
